499df6b3ba2f72f1e3e764a6c039d2ae03b20a95
alok9523 <97608103+alok9523@users.noreply.github.com>
2025-02-25 12:23:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 import os
+from dotenv import load_dotenv
 
 # Load environment variables
+load_dotenv()
+
 WOLFRAM_APP_ID = os.getenv("WOLFRAM_APP_ID")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
