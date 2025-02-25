de44e925218145eb60e98119ad434a45b7998a93
alok9523 <97608103+alok9523@users.noreply.github.com>
2025-02-25 10:12:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-# Telegram Bot Token
+config = { # Telegram Bot Token
 TELEGRAM_TOKEN= "7604560900:AAHWFEFEu3DsovVtYmAiZbj5bL25YLG8nKE"
 
 # Wolfram Alpha API Key
@@ -6,3 +6,4 @@ WOLFRAM_APP_ID = "3GJAG3-3RA3EGRUQ7"  # ✅ Changed to WOLFRAM_APP_ID
 
 # Gemini API Key (Google AI)
 GEMINI_API_KEY = "AIzaSyB59k6rte1ESmAc5ZNVGlxjgUYrNnL7jCc"
+}
