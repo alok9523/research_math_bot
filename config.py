98e12aedf4d7527d3bd4af53a18f7d850ac486a2
alok9523 <97608103+alok9523@users.noreply.github.com>
2025-02-25 10:29:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
-config = { # Telegram Bot Token
-"TELEGRAM_TOKEN": "7604560900:AAHWFEFEu3DsovVtYmAiZbj5bL25YLG8nKE"
+import os
 
-# Wolfram Alpha API Key
-"WOLFRAM_APP_ID": "3GJAG3-3RA3EGRUQ7"
-
-"GEMINI_API_KEY": "AIzaSyB59k6rte1ESmAc5ZNVGlxjgUYrNnL7jCc"
-}
+# Load environment variables or set defaults
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "7604560900:AAHWFEFEu3DsovVtYmAiZbj5bL25YLG8nKE")
+WOLFRAM_APP_ID = os.getenv("WOLFRAM_APP_ID", "3GJAG3-3RA3EGRUQ7")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AIzaSyB59k6rte1ESmAc5ZNVGlxjgUYrNnL7jCc")
