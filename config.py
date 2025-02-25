a355cc215eac89caf0d144d7a5a4b8da2b466009
alok9523 <97608103+alok9523@users.noreply.github.com>
2025-02-25 12:08:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-# Load environment variables or set defaults
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "7604560900:AAHWFEFEu3DsovVtYmAiZbj5bL25YLG8nKE")
-WOLFRAM_APP_ID = os.getenv("WOLFRAM_APP_ID", "3GJAG3-3RA3EGRUQ7")
-GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "AIzaSyB59k6rte1ESmAc5ZNVGlxjgUYrNnL7jCc")
+# Load environment variables
+WOLFRAM_APP_ID = os.getenv("WOLFRAM_APP_ID")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
